072a0a6eff4930c807356d3f568372d2529ee00b
Hamza Khalid <hamza.khalid2477@gmail.com>
2026-05-09 15:46:05 +0500

    Added forgot password and authorization middleware

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,15 +4,15 @@ import bcrypt from "bcryptjs";
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 import { sendToken } from "../utils/jwtToken.js";
+import { generateResetPasswordToken } from "../utils/generateResetPasswordToken.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import { generateForgotPasswordEmailTemplate } from "../utils/generateForgotPasswordEmailTemplate.js";
 
 export const register = catchAsyncErrors(async (req, res, next) => {
-
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    return next(
-      new ErrorHandler("Please provide all required fields.", 400)
-    );
+    return next(new ErrorHandler("Please provide all required fields.", 400));
   }
 
   const normalizedName = name.trim();
@@ -20,24 +20,18 @@ export const register = catchAsyncErrors(async (req, res, next) => {
 
   if (password.trim().length < 8) {
     return next(
-      new ErrorHandler(
-        "Password must be at least 8 characters.",
-        400
-      )
+      new ErrorHandler("Password must be at least 8 characters.", 400),
     );
   }
 
   const isAlreadyRegistered = await database.query(
     `SELECT id FROM users WHERE email = $1`,
-    [normalizedEmail]
+    [normalizedEmail],
   );
 
   if (isAlreadyRegistered.rows.length > 0) {
     return next(
-      new ErrorHandler(
-        "User already registered with this email.",
-        409
-      )
+      new ErrorHandler("User already registered with this email.", 409),
     );
   }
 
@@ -47,17 +41,12 @@ export const register = catchAsyncErrors(async (req, res, next) => {
     `INSERT INTO users(name, email, password)
      VALUES($1, $2, $3)
      RETURNING id, name, email`,
-    [normalizedName, normalizedEmail, hashedPassword]
+    [normalizedName, normalizedEmail, hashedPassword],
   );
 
   const newUser = user.rows[0];
 
-  sendToken(
-    newUser,
-    201,
-    "User registered successfully",
-    res
-  );
+  sendToken(newUser, 201, "User registered successfully", res);
 });
 
 export const login = catchAsyncErrors(async (req, res, next) => {
@@ -69,46 +58,95 @@ export const login = catchAsyncErrors(async (req, res, next) => {
     typeof email !== "string" ||
     typeof password !== "string"
   ) {
-    return next(
-      new ErrorHandler("Please provide email and password", 400)
-    );
+    return next(new ErrorHandler("Please provide email and password", 400));
   }
 
   const normalizedEmail = email.toLowerCase().trim();
 
-  const user = await database.query(
-    "SELECT * FROM users WHERE email = $1",
-    [normalizedEmail]
-  );
+  const user = await database.query("SELECT * FROM users WHERE email = $1", [
+    normalizedEmail,
+  ]);
 
   if (user.rows.length === 0) {
-    return next(
-      new ErrorHandler("Invalid email or password", 401)
-    );
+    return next(new ErrorHandler("Invalid email or password", 401));
   }
 
-  const isPasswordMatch = await bcrypt.compare(
-    password,
-    user.rows[0].password
-  );
+  const isPasswordMatch = await bcrypt.compare(password, user.rows[0].password);
 
   if (!isPasswordMatch) {
-    return next(
-      new ErrorHandler("Invalid email or password", 401)
-    );
+    return next(new ErrorHandler("Invalid email or password", 401));
   }
 
   const loggedInUser = { ...user.rows[0] };
   delete loggedInUser.password;
 
-  sendToken(
-    loggedInUser,
-    200,
-    "Logged in successfully",
-    res
-  );
+  sendToken(loggedInUser, 200, "Logged in successfully", res);
 });
 
+export const getUser = catchAsyncErrors(async (req, res, next) => {
+  const { user } = req;
 
-export const logout = catchAsyncErrors(async (req, res, next) => {});
-export const getUser = catchAsyncErrors(async (req, res, next) => {});
+  res.status(200).json({
+    successs: true,
+    user,
+  });
+});
+
+export const logout = catchAsyncErrors(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+    })
+    .json({
+      success: true,
+      message: "Logged out successfully.",
+    });
+});
+
+export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const { email } = req.body;
+  const { frontendUrl } = req.query;
+  let userResult = await database.query(
+    `SELECT * FROM users WHERE email = $1`,
+    [email],
+  );
+  if (userResult.rows.length === 0) {
+    return next(new ErrorHandler("User not found with this email.", 404));
+  }
+  const user = userResult.rows[0];
+  const { hashedToken, resetPasswordExpireTime, resetToken } =
+    generateResetPasswordToken();
+
+  await database.query(
+    `UPDATE users
+   SET reset_password_token = $1,
+       reset_password_expire = to_timestamp($2)
+   WHERE email = $3`,
+    [hashedToken, resetPasswordExpireTime / 1000, email],
+  );
+
+  const resetPasswordUrl = `${frontendUrl}/password/reset/${resetToken}`;
+
+  const message = generateForgotPasswordEmailTemplate(resetPasswordUrl);
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Ecomerce password Recovery",
+      message,
+    });
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} successfully.`,
+    });
+  } catch (error) {
+    await database.query(
+      `UPADTE users set reset_password_token = NULL,
+       reset_password_expire = NULL WHERE email = $1`,
+      [email],
+    );
+    return next(new ErrorHandler("Email could not be sent",500));
+  }
+});
